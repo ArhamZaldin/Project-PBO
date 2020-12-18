478ae2846c06bb89bf83af72e5e6a7f279d21255
Arham Zainul Abidin <71875242+ArhamZaldin@users.noreply.github.com>
2020-12-19 00:00:02 +0700

    up

--- main.py
+++ main.py
@@ -7,18 +7,29 @@ class dataKeluarga:
         self.alamat = alamat
 
     def show(self):
-        return
+        return "Nomer KK : {} \nKepala keluarga : {} \nNIK : {} \nTotal penghasilan : {} \nAlamat : {}".format(self.noKK, self.kepalaKeluarga, self.nik, self.total, self.alamat)
 
 class anggotaKeluarga(dataKeluarga):
     def __init__(self, noKK, kepalaKeluarga, nik, total, alamat, anggota):
         super().__init__(noKK, kepalaKeluarga, nik, total, alamat)
         self.anggota = anggota
+        self.nama = []
+        self.nikAnggota = []
 
     def anggota(self):
         if self.anggota > 1 :
-            for x in range (len(self.anggota - 1)):
-
+            for x in range(len(self.anggota - 1)):
+                self.nama.append(input("Masukkan nama anggota keluarga {} : ".format(x)))
+                self.nikAnggota.append(input("Masukkan NIK anggota keluarga {} : ".format(x)))
 
     def show(self):
-        return
+        print("Nomer KK : {} \nNama anggota keluarga : {} \nNIK : {}".format(self.noKK, self.kepalaKeluarga, self.nik))
+        for x in self.nikAnggota:
+            return "No KK : {} \nNama anggota keluarga : {} \nNIK : {}".format(self.noKK, self.nama[x], self.nikAnggota[x])
 
+keluarga = dataKeluarga(123, 'anto', 345, 1000000, 'Sumbersari')
+print(keluarga.show())
+
+# keluarga = anggotaKeluarga(123, 'anto', 345, 1000000, 'Sumbersari', 2)
+# keluarga.anggota()
+# print(keluarga.show)
